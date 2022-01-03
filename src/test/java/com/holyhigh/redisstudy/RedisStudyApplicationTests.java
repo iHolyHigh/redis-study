487e67decacc4e391c61f17009f02f931612cedb
HolyHigh <845803085@qq.com>
2022-01-03 23:06:15 +0800

    feat: Redis学习，项目初始化

---
+++ src/test/java/com/holyhigh/redisstudy/RedisStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.holyhigh.redisstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
